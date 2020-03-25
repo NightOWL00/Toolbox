d1a1dc0c0230b6f471548366c0c2e8fcc0c05f94
NightOWL00 <47278518+NightOWL00@users.noreply.github.com>
2020-03-26 04:31:46 +0530

    fixed pdfall,py

--- PDFall/PDFall.py
+++ PDFall/PDFall.py
@@ -1,26 +1,16 @@
-from PIL import Image
 import os
-
-if __name__ == '__main__':
-
-    cwd = os.getcwd()  # For Getting Current Working Directory.
-    l = os.listdir(cwd)  # Making List of FILES in corresponding Directory.
-    img_list = []
-    imagelist = []
-    # Loop to find FILES of images format and making a list of them.
-    for i in l:
-        if '.' in i and len(i) >= 6:
-            if i[-5] == '.' and i[-4:] == 'jpeg':
-                img_list.append(i)
-            elif i[-4] == '.' and (i[-3:] == 'jpg' or i[-3:] == 'png'):
-                img_list.append(i)
-    # Loop to convert Images into a PDF file.
-    for i in img_list:
-        image1 = Image.open(cwd+'\\'+i, "r")
-        im1 = image1.convert('RGB')
-        imagelist.append(im1)
-    image1 = Image.open(cwd+'\\'+img_list[0], "r")
-    # To save the PDF file.
-    imagelist = imagelist[1::]
-    image1.save(cwd+'\ALL_IMAGE_PDF.pdf',
-                save_all=True, append_images=imagelist)
+from PIL import Image
+print("Only Supports PNG, JPEG and JPG files \nCurrent Directory: ", os.listdir())
+tmp = []
+imagelist = [i for i in os.listdir() if i.endswith(
+    ".png") or i.endswith(".jpg") or i.endswith(".jpeg")]
+print(imagelist)
+if len(imagelist) != 0:
+    for i in imagelist[1::]:
+        img = Image.open(i, 'r')
+        tmp.append(img)
+    img = Image.open(imagelist[0], 'r')
+    img.save('ALL_IMAGE_PDF.pdf', save_all=True, append_images=tmp)
+    print(f"Done combining {len(imagelist)} images to a PDF !!")
+else:
+    print("No Supported format found")
