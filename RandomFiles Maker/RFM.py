691327d3dfa767cc534e759503ef7b5a87314362
Kushagra Gangwar <kushagragangwar4@gmail.com>
2020-04-16 20:10:00 +0530

    Update RFM.py
    Changed the code a bit and added required changes.

--- RandomFiles Maker/RFM.py
+++ RandomFiles Maker/RFM.py
@@ -11,16 +11,21 @@ def make_names(file_names):
         tmp.append(i)
     return tmp
 
+def make_files(file_names):
+    for i in file_names:
+        f = open(i,'w+')
+        f.close
 
 alpha = string.ascii_letters
 number = int(input('Enter number of files to be created: '))
 f = ''
 file_names = []
 for j in range(number):
-    for i in range(13):
+    for i in range(7):
         x = random.randint(0, len(alpha)-1)
         f = f+alpha[x]
     file_names.append(f)
     f = ''
 
 file_names = make_names(file_names)
+make_files(file_names)
